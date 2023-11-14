3ac381d3e897e327953b0900be9dc450c60c596f
Adrien Simon <adrisim2001@gmail.com>
2023-11-14 23:59:04 +0100

    ouais

---
+++ project/app/Enums/MemberRole.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Enums;
+
+enum MemberRole : string
+{
+    case Administrator = 'admin';
+    case Member = 'member';
+    case None = 'none';
+
+    public function isAdministrator(): bool
+    {
+        return $this === self::Administrator;
+    }
+
+    public function isMember(): bool
+    {
+        return $this === self::Member;
+    }
+
+    public function title(): string
+    {
+        return match ($this) {
+            self::Administrator => 'Administrateur',
+            self::Member => 'Membre',
+            self::None => 'Aucun droit',
+        };
+    }
+}
