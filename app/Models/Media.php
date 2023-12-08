27e80f8790b6df1bbc85f385357e17e2471bb1a6
noams0 <141066465+noams0@users.noreply.github.com>
2023-12-08 10:50:52 +0100

    KAN-27 : TV (#16)
    * reorganisation
    
    * bug réglé et media sans durée
    
    * ajout enum
    
    * controllers documentés
    
    * documentation
    
    * Delete database.sqlite

---
+++ app/Models/Media.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Enums\MediaType;
+
+class Media extends Model
+{
+    use HasFactory;
+    protected $table = 'media';
+
+    protected $fillable = [
+        'name',
+        'media_type',
+        'media_path',
+        'activated',
+        'times',
+    ];
+
+    protected $casts = [
+        'media_type' => MediaType::class,
+    ];
+}
