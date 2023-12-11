3493b60ff207518861561d8918604e36dc046708
noams <noam.seuret@etu.utc.fr>
2023-12-11 17:05:01 +0100

    fix

--- resources/views/creneau/listeCreneaux.blade.php
+++ resources/views/creneau/listeCreneaux.blade.php
@@ -37,15 +37,17 @@
                <br>
             @foreach($creneauxDate as $creneau)
                 {{$creneau->creneau}}
-                <form action="{{ route('creneau.associate-perm',  $creneau)}}" method="post">
+                <form action="{{ route('creneau.associate-perm',  $creneau, )}}" method="post">
                     @csrf
                     <select name="perm_id">
+                        <option value="" selected></option>
                         @foreach($perms as $perm)
                             <option value="{{ $perm->id }}" {{ $creneau->perm_id == $perm->id ? 'selected' : '' }}>
                                 {{ $perm->nom }} - {{ $perm->theme }}
                             </option>
                         @endforeach
                     </select>
+
                     <button type="submit">Associer</button>
                 </form>
             @endforeach
