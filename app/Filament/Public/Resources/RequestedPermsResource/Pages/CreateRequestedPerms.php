6235c56e3aafa427e9ead9e8666bcbbd5acc06f7
noams <noam.seuret@etu.utc.fr>
2024-08-25 10:32:15 +0200

    redirection after create perm

--- app/Filament/Public/Resources/RequestedPermsResource/Pages/CreateRequestedPerms.php
+++ app/Filament/Public/Resources/RequestedPermsResource/Pages/CreateRequestedPerms.php
@@ -10,4 +10,5 @@ class CreateRequestedPerms extends CreateRecord
 {
     protected static string $resource = RequestedPermsResource::class;
     protected static ?string $title = 'Demander une permanence';
+    protected static bool $canCreateAnother = false;
 }
