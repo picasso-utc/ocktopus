c055eabb70d278fffa63eb7553a3761c259a989f
noams <noamseuret@gmail.com>
2024-11-04 20:15:18 +0100

    fix it

--- app/Filament/Public/Resources/ExteResource/Pages/ListExtes.php
+++ app/Filament/Public/Resources/ExteResource/Pages/ListExtes.php
@@ -3,13 +3,21 @@
 namespace App\Filament\Public\Resources\ExteResource\Pages;
 
 use App\Filament\Public\Resources\ExteResource;
+use App\Models\Exte;
 use Filament\Actions;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListExtes extends ListRecords
 {
     protected static string $resource = ExteResource::class;
 
+    protected function getTableQuery(): ?Builder
+    {
+        return Exte::query()
+            ->where('etu_mail', Filament::auth()->user()?->email);
+    }
     protected function getHeaderActions(): array
     {
         return [
--- resources/views/emails/mail_exte_confirmation.blade.php
+++ resources/views/emails/mail_exte_confirmation.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <p>Hello {{ $record->etu_nom_prenom }} !</p>
-<p>Nous t'envoyons ce mail pour te dire que ta demande d'exté pour <strong>{{ $record->exte_nom_prenom }}</strong> du <strong> {{Carbon::parse($record->exte_date_debut)->translatedFormat('d F Y') }} au {{Carbon::parse($record->exte_date_fin)->translatedFormat('d F Y')  }}</strong></p>
+<p>Nous t'envoyons ce mail pour te dire que ta demande d'exté pour <strong>{{ $record->exte_nom_prenom }}</strong> du <strong> {{Carbon::parse($record->exte_date_debut)->translatedFormat('d F Y') }} au {{Carbon::parse($record->exte_date_fin)->translatedFormat('d F Y')  }}</strong> a bien été validée</p>
 <p>Pour que ton exté rentre, il faudra qu'iel rentre <b>en ta compagnie</b> absolument avec une <b>pièce d'identité</b> ainsi que <b>ce mail</b> attestant que la venue de ton exté est autorisée.</p>
 
 <p>Bonne journée</p>
