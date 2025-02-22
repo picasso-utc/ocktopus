c716230686c9f0b2113f49b5ea6b09b811800710
geosaglio <geosaglioo@gmail.com>
2025-02-22 16:29:09 +0100

    Change rejection mail content

--- resources/views/emails/mail_refus_exte.blade.php
+++ resources/views/emails/mail_refus_exte.blade.php
@@ -9,7 +9,7 @@
 <body>
 <p>Hello {{ $record->etu_nom_prenom }} !</p>
 <p>Nous t'envoyons ce mail pour te dire que ta demande d'exté pour <strong>{{ $record->exte_nom_prenom }}</strong> du <strong> {{Carbon::parse($record->exte_date_debut)->translatedFormat('d F Y') }} au {{Carbon::parse($record->exte_date_fin)->translatedFormat('d F Y')  }}</strong> a été refusée.</p>
-<p>La raison de ce rejet peut venir de l'administration ou bien d'une demande mal remplie (manque du prénom et/ou du nom de votre exté, cas non renseigné, ...). Si vous avez des questions vis-à-vis de ce refus, n'hésitez pas à répondre à ce mail ou à nous envoyer un message sur Instagram !</p>
+<p>La raison de ce rejet peut venir de l'administration ou bien d'une demande mal remplie (manque du prénom et/ou du nom de votre exté, cas non renseigné, ...). Si tu as des questions vis-à-vis de ce refus, n'hésite pas à répondre à ce mail ou à nous envoyer un message sur Instagram !</p>
 
 <p>Bonne journée</p>
 <strong>
