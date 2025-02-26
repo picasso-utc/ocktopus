13331eda8a4d8abd21f025df1855d6630f415eaa
mathisDlmr <mathis.delmaere@free.fr>
2025-02-26 20:56:27 +0100

    Modification analyse factures pour prendre en compte les recettes

--- app/Filament/Treso/Pages/AnalyseFactures.php
+++ app/Filament/Treso/Pages/AnalyseFactures.php
@@ -2,22 +2,24 @@
 
 namespace App\Filament\Treso\Pages;
 
+use App\Models\CategorieFacture;
 use App\Models\MontantCategorie;
 use App\Models\FactureRecue;
-use Carbon\Carbon;
+use App\Models\Recettes;
 use Filament\Pages\Page;
 use Filament\Forms\Components\DatePicker;
 
 class AnalyseFactures extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-chart-bar';
-    protected static ?string $title = 'Analyse des Factures Reçues';
+    protected static ?string $title = 'Analyse Générale';
     protected static string $view = 'filament.treso.pages.analyse-factures';
 
     public $date_debut;
     public $date_fin;
-    public $total = 0;
     public $totalsByCategory = [];
+    public $totalRecettes = 0;
+    public $totalDepenses = 0;
 
     protected function getFormSchema(): array
     {
@@ -33,18 +35,51 @@ class AnalyseFactures extends Page
 
     public function calculerTotal()
     {
-        $this->total = FactureRecue::whereBetween('date', [$this->date_debut, $this->date_fin])
+        $this->totalDepenses = FactureRecue::whereBetween('date', [$this->date_debut, $this->date_fin])
             ->sum('prix');
+
+        $this->totalRecettes = Recettes::whereBetween('date_fin', [$this->date_debut, $this->date_fin])  // On se base sur la date de fin de la recette pour savoir si on la prend en compte ou pas
+            ->sum('valeur');
     }
 
-    public function calculerTotals()
+    public function calculerTotaux()
     {
-        $this->totalsByCategory = MontantCategorie::with('categorie')
+        $recettesByCategory = CategorieFacture::with(['recettes' => function ($query) {  // Calcule les recettes par catégorie
+                $query->whereBetween('date_debut', [$this->date_debut, $this->date_fin]);
+            }])
+            ->get()
+            ->mapWithKeys(function ($categorie) {
+                $totalRecettes = $categorie->recettes->sum('valeur');
+                return [
+                    $categorie->id => [
+                        'categorie' => $categorie->nom,
+                        'totalRecettes' => $totalRecettes,
+                        'totalDepenses' => 0, // On init pour l'instant à 0, le calcul est fait apr.
+                    ],
+                ];
+            })
+            ->toArray(); // Convertir en tableau
+
+        $depensesByCategory = MontantCategorie::with('categorie')   // calcule maintenant les dépenses (factures reçues) par catégories
             ->whereHas('facture', function ($query) {
                 $query->whereBetween('date', [$this->date_debut, $this->date_fin]);
             })
             ->selectRaw('categorie_id, SUM(prix) as total_prix')
             ->groupBy('categorie_id')
             ->get();
+
+        foreach ($depensesByCategory as $depense) {    // fusionner les res dans un array
+            if (isset($recettesByCategory[$depense->categorie_id])) {
+                $recettesByCategory[$depense->categorie_id]['totalDepenses'] = $depense->total_prix;
+            } else {
+                $recettesByCategory[$depense->categorie_id] = [
+                    'categorie' => $depense->categorie->nom,
+                    'totalRecettes' => 0,
+                    'totalDepenses' => $depense->total_prix,
+                ];
+            }
+        }
+
+        $this->totalsByCategory = array_values($recettesByCategory);
     }
 }
