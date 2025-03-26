7f6625e50b5e1204e031d09c9e3464dcc86dba29
mathisDlmr <140653974+mathisDlmr@users.noreply.github.com>
2025-03-26 15:34:39 +0100

    Update install.sh

--- storage/app/files/install.sh
+++ storage/app/files/install.sh
@@ -3,6 +3,8 @@
 # It also creates a user for the service and sets the service to start at boot
 # It install also the chromium browser
 
+# Avant toute chose : installer le pilote du lecteur de carte. Attention a l'arch du rasp
+
 sudo apt update && sudo apt upgrade -y
 
 # Install pcsc and lib
@@ -17,13 +19,15 @@ sudo apt-get install chromium-browser -y
 # download the jcapuchino.jar file at https://pic.assos.utc.fr/download/jcapuchino.jar
 # and place it in the /var/jcapuchino/ folder
 sudo mkdir /var/jcapuchino
-wget -O /var/jcapuchino/jcapuchino.jar https://pic.assos.utc.fr/download/jcapuchino.jar
+wget -O /var/jcapuchino/jcapuchino.jar https://pic.assos.utc.fr/download/jcapuchino.jar # Verifier que jcapucino.jar a bien ete telecharge au bon endroit
 
 # Create a user for the service
 sudo useradd -r -s /bin/false jcapuchino
 
 # Create a service file
-sudo touch /etc/systemd/system/jcapuchino.service
+sudo touch /etc/systemd/system/jcapuchino.service # Il peut arriver que le service soit masque. 
+                                                  # (verifier avec sudo systemctl is-enabled jcapuchino.service, et 
+                                                  # demasquer avec sudo systemctl unmask jcapuchino.service si necessaire)
 sudo echo "[Unit]
 Description=JCapuchino
 After=network.target
@@ -35,7 +39,7 @@ ExecStart=/usr/bin/java -jar /var/jcapuchino/jcapuchino.jar
 Restart=on-failure
 
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/jcapuchino.service
+WantedBy=multi-user.target" > /etc/systemd/system/jcapuchino.service # Il peut arriver que le echo ne reussisse pas a ecrire -> Faire a la main
 
 # Enable the service
 sudo systemctl daemon-reload
@@ -44,4 +48,4 @@ sudo systemctl start jcapuchino.service
 sudo systemctl status jcapuchino.service
 
 # add chromium to the autostart of "https://pic.assos.utc.fr/bach/" raspbian os (start in kiosk mode incognito)
-sudo echo "chromium-browser https://pic.assos.utc.fr/bach/" >> /etc/xdg/lxsession/LXDE-pi/autostart
+sudo echo "chromium-browser https://pic.assos.utc.fr/bach/" >> /etc/xdg/lxsession/LXDE-pi/autostart # Cette methode ne fonctionne plus sur les Rasp > 3 (trouver une solution)
